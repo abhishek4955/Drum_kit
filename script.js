10a09e87b24d13f8137a10bb4b17fcbdcfc808ff
abhishek4955 <rohitkhadse450@gmail.com>
2023-07-09 05:36:23 +0530

    Ill be adding index.html file

---
+++ script.js
@@ -0,0 +1,46 @@
+window.onload = function() {
+  // Retrieve all the elements with class "drum"
+  var buttons = document.getElementsByClassName("drum");
+
+  // Add click event listeners to each button
+  for (var i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener("click", function() {
+      var buttonInnerHTML = this.innerHTML;
+      playDrumSound(buttonInnerHTML);
+    });
+  }
+};
+
+  // Function to play Drum Sounds
+function playDrumSound(key) {
+  var audio = new Audio();
+  
+  // Set the audio source based on the button key
+  switch (key) {
+    case "w":
+      audio.src = "./audio/drum-1.mp3";
+      break;
+    case "a":
+      audio.src = "./audio/drum-2.mp3";
+      break;
+    case "s":
+      audio.src = "./audio/drum-3.mp3";
+      break;
+    case "d":
+      audio.src = "./audio/drum-4.mp3";
+      break;
+    case "j":
+      audio.src = "./audio/drum-5.mp3";
+      break;
+    case "k":
+      audio.src = "./audio/drum-6.mp3";
+      break;
+    case "l":
+      audio.src = "./audio/drum-7.mp3";
+      break;
+    default:
+      return;
+  }
+
+  audio.play();
+}
